959d4f6e5c80de4becc7cf289d5d334e8a4aac27
PunitMukeshAhuja <punitmukeshahuja03@gmail.com>
2019-07-17 10:02:08 -0400

    User story 3

--- src/main/java/com/cs/rfq/decorator/extractors/RfqMetadataFieldNames.java
+++ src/main/java/com/cs/rfq/decorator/extractors/RfqMetadataFieldNames.java
@@ -8,6 +8,7 @@ public enum RfqMetadataFieldNames {
     tradesWithEntityPastWeek,
     tradesWithEntityPastYear,
     volumeTradedYearToDate,
+    userTradePercentage,
     volumeTradedPastYear,
     volumeTradedPastMonth,
     volumeTradedPastWeek
