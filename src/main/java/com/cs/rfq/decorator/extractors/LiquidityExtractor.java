323eb1fe58dcb49ebff770e17142e4c8eed92884
rajdeepiitkgp <rajdeep.biswas2010@gmail.com>
2019-07-17 06:54:26 -0400

    liquidity extractor

--- src/main/java/com/cs/rfq/decorator/extractors/LiquidityExtractor.java
+++ src/main/java/com/cs/rfq/decorator/extractors/LiquidityExtractor.java
@@ -7,24 +7,32 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.joda.time.DateTime;
 
+import java.sql.Date;
 import java.util.Map;
 
 public class LiquidityExtractor {
     private Long since;
     private Dataset<Row> trades;
 
-    public boolean checkLiquidity(DateTime dateTo,Long threshold,Rfq rfq,SparkSession session){
+    public boolean checkLiquidity(DateTime dateTo, Long threshold, Rfq rfq, SparkSession session){
 
         long todayMs = dateTo.withMillisOfDay(0).getMillis();
         long since = dateTo.withMillis(todayMs).minusWeeks(1).getMillis();
+        //long todayMs = DateTime.now().withMillisOfDay(0).getMillis();
+        //long pastWeekMs = DateTime.now().withMillis(todayMs).minusWeeks(1).getMillis();
+        //long difference = todayMs - since;
+        //System.out.println("difference " +difference);
 
         trades = new TradeDataLoader().loadTrades(session, "src/test/resources/trades/successful-trades.json");
 
         Dataset<Row> filtered = trades
                 .filter(trades.col("SecurityId").equalTo(rfq.getIsin()));
+        //System.out.println(filtered.count());
+        //System.out.println("sec id "+rfq.getIsin());
 
 
         long tradesPastWeek = filtered.filter(trades.col("TradeDate").$greater(new java.sql.Date(since))).count();
+        ////System.out.println(tradesPastWeek);
         if(tradesPastWeek < threshold)
             return false;
         else
